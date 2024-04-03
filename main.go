e6179f52a5d48a7e8081133c3df2b26d5ce35f5d
Conor Lewis-Hobbs <105722475+ConorLewis-Hobbs@users.noreply.github.com>
2024-04-03 18:51:53 +0200

    added to txt

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+
+}
 
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Create a new bill named: ")
-	name, _ := reader.ReadString('\n')
-	//set user input to name and save it when they start a new line
-	//such as on enter
-	name = strings.TrimSpace(name)
-	//trims white space from user input
+	name, _ := getInput("Create a new bill named: ", reader)
 
 	b := newBill(name)
 	fmt.Println("Created new bill named: ", b.name)
@@ -23,10 +27,51 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (a - add item, t - add tip, s - save bill) ", reader)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number...")
+			promptOptions(b)
+		} else {
+			b.addItem(name, p)
+			fmt.Println("Item added - ", name, price)
+			promptOptions(b)
+		}
+
+	case "t":
+		tip, _ := getInput("Enter tip ammount: (£) ", reader)
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number...")
+			promptOptions(b)
+		} else {
+			b.updateTip(t)
+			fmt.Println("Tip added - ", tip)
+			promptOptions(b)
+		}
+
+	case "s":
+		b.save()
+		fmt.Println("You chose to save the bill in file - ", b.name)
+	default:
+		fmt.Println("Entered invalid option...")
+		promptOptions(b)
+	}
+}
+
 func main() {
 	mybill := createBill()
+	promptOptions(mybill)
 
-	fmt.Println(mybill)
+	// fmt.Println(mybill)
 }
 
 // mybill := newBill("mario's bill")
